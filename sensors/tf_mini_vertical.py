e4b8c14b0640be315c74b765780a7cc770ec0b20
GaboPulidoM <Gabriel-Fernando.PULIDO-MARINO@eleves.enpc.fr>
2023-04-04 17:59:42 +0200

    tf_mini_vertical finie, il faut le tester

--- sensors/tf_mini_vertical.py
+++ sensors/tf_mini_vertical.py
@@ -9,36 +9,38 @@ class DroneVerticalSensors(object):
     0 if is below the drone
     """
 
-    def __init__(self, lidar_address, lidar_position, critical_distance_lidar = 100):
+    def __init__(self, lidar_address, lidar_position, critical_distance_lidar=100):
         self._critical_distance_lidar = critical_distance_lidar
         # Initialize a list with vertical lidar sensors
         self.lidar_v_sensors = self._init_lidar_v_sensors(lidar_address, lidar_position)
-
 
     def _init_lidar_v_sensors(self, lidar_address, lidar_position):
         """
         Initialize a list of class tfmini sensors for vertical position
         """
-        lidar_v_sensor = [] # List of vertical sensors
+        lidar_v_sensor = []  # List of vertical sensors
         if lidar_address is not None:
-            index = dict(zip(lidar_address, lidar_position) # Dictionary of vertical lidars
+            index = dict(zip(lidar_address, lidar_position))  # Dictionary of vertical Lidars
             for address, position in index.items():
-                lidar_v_sensor.append(TFMiniPlus(address, v_position=position, self._critical_distance_lidar))
+                lidar_v_sensor.append(TFMiniPlus(address, [0], position, self._critical_distance_lidar))
         return lidar_v_sensor
+
 
 class VerticalLidarsDetection(object):
     """
     Class to organize and identify the vertical lidars
     """
 
-    def __init__(self,lidar_address, lidar_position, critical_distance_lidar = 100):
-        self._lidar_sensors = DroneVerticalSensors(lidar_address, lidar_position, critical_distance_lidar = 100).lidar_v_sensors
+    def __init__(self, lidar_address, lidar_position, critical_distance_lidar=100):
+        self._lidar_sensors = DroneVerticalSensors(lidar_address, lidar_position, critical_distance_lidar).lidar_v_sensors
         self._up_lidar = None
         self._down_lidar = None
         self._distance_up = None
         self._distance_down = None
         self._go_up = False
         self._go_down = False
+        self._obstacle_detected_up = False
+        self._obstacle_detected_down = False
 
         self._identify_sensors()
 
@@ -46,3 +48,73 @@ class VerticalLidarsDetection(object):
         """
         Function to organize the vertical lidars
         """
+        for lidar in self._lidar_sensors:
+            if lidar.angle == 1:
+                lidar.name = "Up lidar"
+                self._up_lidar = lidar
+            else:
+                lidar.name = "Down lidar"
+                self._down_lidar = lidar
+
+    # Functions to access the different lidars
+    def get_up_lidar(self):
+        return self._up_lidar
+
+    def get_down_lidar(self):
+        return self._down_lidar
+
+    # Functions to obtain the distances measure by the lidars
+
+    def read_up_distance(self):
+        self.get_up_lidar().read_distance()
+
+    def get_up_distance(self):
+        self._distance_up = self.get_up_lidar().get_distance()
+        return self._distance_up
+
+    def read_down_distance(self):
+        self.get_down_lidar().read_distance()
+
+    def get_down_distance(self):
+        self._distance_down = self.get_down_lidar().get_distance()
+        return self._distance_down
+
+    # Functions to get data of obstacle and command of up or down
+
+    def obstacle_detected_up(self):
+        return self._obstacle_detected_up
+
+    def obstacle_detected_down(self):
+        return self._obstacle_detected_down
+
+    # Function to send the direction to go to the drone
+    def update_vertical_path(self):
+        """
+        Function used to update the vertical path
+        """
+        self.read_up_distance()
+        self.read_down_distance()
+        if self.get_up_distance() > self.get_down_distance():
+            self._go_up = True
+            self._go_down = False
+        elif self.get_up_distance() < self.get_down_distance():
+            self._go_down = True
+            self._go_up = False
+        else:
+            self._go_down = False
+            self._go_up = False
+
+    def update_vertical_path_obstacle(self, vertical_obstacle_detected):
+        """
+        Function used to update the vertical path if there's
+        an obstacle(ceiling or floor closer than distance critique)
+        """
+        if vertical_obstacle_detected:
+            if self._obstacle_detected_up:
+                self._go_up = False
+            if self._obstacle_detected_down:
+                self._go_down = False
+
+
+
+
