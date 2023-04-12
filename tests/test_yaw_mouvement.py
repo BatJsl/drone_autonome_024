0a56a4c661753ec0540fa90554918fc69c0682db
GaboPulidoM <Gabriel-Fernando.PULIDO-MARINO@eleves.enpc.fr>
2023-04-12 10:48:03 +0200

    modification test yaw rotation code SITL, speed rotation in virtual inspection drone fichier

--- tests/test_yaw_mouvement.py
+++ tests/test_yaw_mouvement.py
@@ -62,10 +62,12 @@ while drone.mission_running():
     if drone.vert_lidar._go_up:
         print("rotating CW")
         drone.send_mavlink_right_rotate(30)
+        time.sleep(3)
 
     elif drone.vert_lidar._go_down:
         print("rotating CCW")
         drone.send_mavlink_left_rotate(30)
+        time.sleep(3)
 
     #time.sleep(2)
 
