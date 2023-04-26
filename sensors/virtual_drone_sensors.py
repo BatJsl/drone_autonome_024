b134cfaaae23ef3c1dd6c9f67a2eb8abf1946d79
HelieD <hduf1709@gmail.com>
2023-04-26 10:25:05 +0200

    To test 3

--- sensors/virtual_drone_sensors.py
+++ sensors/virtual_drone_sensors.py
@@ -2,6 +2,17 @@ from virtual_tf_mini import VirtualTFMiniPlus
 from drone_sensors import DroneLidarSensors
 
 
+def _init_lidar_sensors(lidar_angle):
+    """
+    Initialize a list of lidar sensors with their address and angle
+    """
+    lidar_sensors = []  # List of lidar sensors
+    if lidar_angle is not None:
+        for angle in lidar_angle:
+            # Initialize a lidar
+            lidar_sensors.append(VirtualTFMiniPlus(angle))
+    return lidar_sensors
+
 
 class VirtualDroneLidarSensors(DroneLidarSensors):
     """
@@ -9,21 +20,8 @@ class VirtualDroneLidarSensors(DroneLidarSensors):
     Used to deal with multiple sensors on the drone
     """
     def __init__(self, lidar_angle):
+        DroneLidarSensors.__init__(_init_lidar_sensors(lidar_angle))
 
-        self._lidar_number = len(lidar_angle)  # Number of lidar sensors
-        # Initialize a list with all the lidar sensors
-        self.lidar_sensors = self._init_lidar_sensors(lidar_angle)
-        DroneLidarSensors.__init__(self.lidar_sensors)
-    def _init_lidar_sensors(self, lidar_angle):
-        """
-        Initialize a list of lidar sensors with their address and angle
-        """
-        lidar_sensors = []  # List of lidar sensors
-        if lidar_angle is not None:
-            for angle in lidar_angle:
-                # Initialize a lidar
-                lidar_sensors.append(VirtualTFMiniPlus(angle))
-        return lidar_sensors
 
     def read_distances(self, drone_x, drone_y, drone_angle, walls):
         for i in range(self._lidar_number):
