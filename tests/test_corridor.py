ae0aac95a9eb63a8542aed570839bac444254872
HelieD <hduf1709@gmail.com>
2023-05-16 10:51:50 +0200

    To test 10

--- tests/test_corridor.py
+++ tests/test_corridor.py
@@ -63,7 +63,7 @@ while drone.mission_running():
     drone.update_switch_states()  # update the RC transmitter switch state
     if drone.do_lidar_reading():  # ask a reading every 20 ms
         if simulation:
-            drone.update_detection(use_lidar=True, debug=True, walls=walls)  # distance measure
+            drone.update_detection(use_lidar=True, debug=False, walls=walls)  # distance measure
         else:
             drone.update_detection(use_lidar=True, debug=True)  # distance measure
     if drone.corridor_detected() and drone.is_in_auto_mode():  # corridor detected IRL
